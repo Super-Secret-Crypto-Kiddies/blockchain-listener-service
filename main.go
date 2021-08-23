43f489dbbb76a9962f5d7679f748a7a999e6cfcd
turtlebasket <turtlebasket@gmail.com>
2021-08-22 20:55:14 -0700

    remove eth wallet package (was interfering with current wallet pkg)

--- main.go
+++ main.go
@@ -2,39 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 )
 
 func main() {
 
 	godotenv.Load()
+	fmt.Printf("hi")
 
-	wallet, err := hdwallet.NewFromMnemonic(os.Getenv("SEED"))
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(wallet)
-
-	app := fiber.New(fiber.Config{Prefork: true})
-
-	app.Post("/create-payment-session", func(c *fiber.Ctx) error {
-
-		payload := struct {
-			Crypto string `json:"crypto"` // Ticker symol of the cryptocurrency, e.g. ETH
-			Meta   string `json:"meta"`   // Metadata includes merchant specified data, e.g. userId or productId
-		}{}
-
-		if err := c.BodyParser(&payload); err != nil {
-			return c.SendStatus(500) // Return status 500 if the JSON payload is not unserializable
-		}
-
-		return c.SendStatus(200)
-	})
-
-	app.Listen("localhost:1337")
 }
